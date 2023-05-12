b2e1a7d0c617ccf61f56fe311c17fa9d6a959133
Tahni Hresko <tahni.jae@gmail.com>
2023-05-12 14:32:55 -0400

    added in assets and created spaceship

--- README.md
+++ README.md
@@ -2,17 +2,26 @@
 
 
 ## To-dos
-[x] 1. Create a player
-[ ] 2. Move the player
-[ ] 3. Create projectiles
-[ ] 4. 
-[ ] 5.
-[ ] 6.
-[ ] 7.
-[ ] 8.
-[ ] 9. 
-[ ] 10.
-[ ] 11.
-[ ] 12.
-[ ] 13.
-[ ] 14.
+- [x] 1. Project Setup
+- [ ] 2. Create a player
+- [ ] 3. Move the player
+- [ ] 4. Create projectiles
+- [ ] 5. Create an invader
+- [ ] 6. Create and move grids of invaders
+- [ ] 7. Spawn grids at intervals
+- [ ] 8. Shoot invaders
+- [ ] 9. Invaders shoot back
+- [ ] 10. Enemy Explosions
+- [ ] 11. Create background
+- [ ] 12. Lose condition
+- [ ] 13. Score
+- [ ] 14. Fixed-width canvase
+
+## Roadmap
+- [ ] Dynamic score labels
+- [ ] Bombs
+- [ ] Power ups
+- [ ] Progressively harder enemies
+- [ ] Spaceship sprite animation
+- [ ] UI for start and game over
+- [ ] Sound effects / music
--- index.js
+++ index.js
@@ -1,5 +1,48 @@
+//SETUP
 const canvas = document.querySelector('canvas')
 const canvasContext = canvas.getContext('2d')
 
 canvas.width = innerWidth  // can also do window.innerWidth
 canvas.height = innerHeight
+
+class Player {
+    constructor() { //Initialize player size, location, velocity
+        this.position = {
+            x: 200,
+            y: 200
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = "./assets/img/spaceship.png"
+
+        this.image = image
+        this.width = 100
+        this.height = 100
+    }
+
+    draw() {
+        // canvasContext.fillStyle = 'blue'
+        // canvasContext.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        canvasContext.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+}
+
+const player = new Player()
+player.draw()
+animate()
+
+//Currenty we are only calling the spaceship image once and the screen is loaded in before the image is finished loading so we cannot see it.
+// Because we want to get the spaceship to move and continue to update it's position, it's going to call itself
+function animate (){
+    requestAnimationFrame(animate)
+    canvasContext.fillStyle = 'black'
+    canvasContext.fillRect(0,0, canvas.width, canvas.height)
+    player.draw()
+}
